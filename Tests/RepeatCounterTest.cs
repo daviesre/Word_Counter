e9d9763f91dd2638ba9a0faeae49904d385f7ab8
Russ Davies <russdavies392@gmail.com>
2016-07-22 11:59:03 -0700

    Add first test method

---
+++ Tests/RepeatCounterTest.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+
+namespace WordCounter
+{
+  public class WordCounterTest
+  {
+    [Fact]
+    public void LetterCount_ReturnsOneAfterInputLetter_ReturnsOne()
+    {
+      char myLetter = 'A';
+    }
+  }
+}
