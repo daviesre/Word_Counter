4440e6d5dc1fde24a9d02bcd52379f075e7f3825
Russ Davies <russdavies392@gmail.com>
2016-07-23 09:29:00 -0700

    add minor edits to files

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -43,42 +43,18 @@ namespace WordCounter
       return _numberOfWords;
     }
 
-    //Methods
-    // public int LetterCount(char myLetter)
-    // {
-    //   return 1;
-    // }
-    //
-    // public int WordCount(string myWord)
-    // {
-    //   return 1;
-    // }
-    //
-    // public int LetterAmount(string inputletter, string sentence)
-    // {
-    //   string[] lettersArray = sentence.Split(' ');
-    //   int numberOfLetter = 0;
-    //
-    //   foreach(var letter in lettersArray)
-    //   {
-    //     if (letter == inputletter)
-    //     {
-    //       numberOfLetter++;
-    //     }
-    //   }
-    //   return numberOfLetter;
-    // }
+    //method
 
     public int CountRepeats(string inputword, string inputsentence)
     {
-      // inputsentence=inputsentence.Replace("!","");
-      // inputsentence=inputsentence.Replace("?","");
-      // inputsentence=inputsentence.Replace(".","");
-      // inputsentence=inputsentence.Replace(",","");
-      // inputsentence=inputsentence.Replace(";","");
-      // inputsentence=inputsentence.Replace(":","");
-      // inputsentence=inputsentence.Replace("'","");
-      // inputsentence=inputsentence.Replace("-","");
+      inputsentence=inputsentence.Replace("!","");
+      inputsentence=inputsentence.Replace("?","");
+      inputsentence=inputsentence.Replace(".","");
+      inputsentence=inputsentence.Replace(",","");
+      inputsentence=inputsentence.Replace(";","");
+      inputsentence=inputsentence.Replace(":","");
+      inputsentence=inputsentence.Replace("'","");
+      inputsentence=inputsentence.Replace("-","");
 
       string noCaps = inputsentence.ToLower();
       string noCapsWord = inputword.ToLower();
